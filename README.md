af643c7dcc317a6f54dac18af4c44a42970dbf52
Joseph Cauble <joseph@josephcauble.com>
2021-09-12 21:10:54 -0500

    Clean up code and add documentation

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# whois-server
+# WHOIS Lookup Server
+## Introduction
+This Node.js application interfaces with the [WHOIS XML API](https://main.whoisxmlapi.com/) service by sanitizing an input URL or IP address and returning the resultant WHOIS information.
+
+## How to Build
+### Prerequesites
+* Node.JS v. 14 or higher  
+OR
+* Docker
+* Docker Compose
+  
+You must also create a .env file in the root of this project with your API Key.
+See `example.env`.
+### Docker
+If you have Docker installed simply run `docker-compose up -d --build`.  
+You can then access the API on `localhost:3030`.
+### Node
+If you don't have Docker or would prefer to run the server directly, run `npm install` followed by `npm start`.  
+You can then access the API on `localhost:3030`.
--- src/url-utils.ts
+++ src/url-utils.ts
@@ -1,3 +1,18 @@
+/** 
+ * Utility file for sanitizing and manipulating urls
+ * 
+ * @author Joseph Cauble (josephdc96)
+ */
+
+
+/**
+ * Main sanitize function. This method trims whitespace and 
+ * passes the url to the protocol and path removal methods if necessary.
+ * This results in a URL that the WHOIS XML API can use.
+ * 
+ * @param url The url string to be sanitized
+ * @returns A sanitized url that the API can use
+ */
 function sanitizeURL(url: string): string {
     url = url.trim();
     if (url.indexOf('://') > -1) {
@@ -9,12 +24,24 @@ function sanitizeURL(url: string): string {
     return url;
 }
 
+/**
+ * Strips the protocol (http, https, etc.) from a url
+ * 
+ * @param url The url to be fixed
+ * @returns The url sans protocol
+ */
 function removeProtocol(url: string): string {
     const colonIndex = url.indexOf('://') + 3;
     url = url.substr(colonIndex, url.length - colonIndex);
     return url;
 }
 
+/**
+ * Removes any path from the url beyond the FQDN
+ * 
+ * @param url The url to be fixed
+ * @returns The FQDN only
+ */
 function removePath(url: string): string {
     const slashIndex = url.indexOf('/');
     url = url.substr(0, slashIndex);
